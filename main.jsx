acaef17d876d03db9b2f114f191e95a6ac117e59
sitiovoodosgansos-cpu <sitiovoodosgansos@gmail.com>
2026-03-05 18:48:16 -0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "https://esm.sh/react@18";
+import ReactDOM from "https://esm.sh/react-dom@18/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
